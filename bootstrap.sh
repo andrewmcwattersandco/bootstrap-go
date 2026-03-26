2efbaa4437ccd3260d7cc7bf1ae1e42a796be995
Andrew McWatters <andrew@andrewmcwatters.com>
2026-03-26 15:20:13 -0700

    Add sudo to commands for removing and extracting Go installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@
 
 # https://go.dev/doc/install
 curl -OL https://go.dev/dl/go1.26.1.linux-amd64.tar.gz
- rm -rf /usr/local/go && tar -C /usr/local -xzf go1.26.1.linux-amd64.tar.gz
+ sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.26.1.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> "$HOME/.profile"
  go version
 
